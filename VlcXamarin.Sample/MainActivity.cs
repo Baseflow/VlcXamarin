5bf9633a293c98878f395260b218836747322269
Martijn van Dijk <mhvdijk@gmail.com>
2015-09-09 17:04:45 +0200

    Add sample

---
+++ VlcXamarin.Sample/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Org.Videolan.Libvlc;
+
+namespace VlcXamarin.Sample
+{
+    [Activity (Label = "VlcXamarin.Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        LibVLC mLibVLC = null;
+
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button> (Resource.Id.myButton);
+            
+            button.Click += delegate {
+                button.Text = string.Format ("{0} clicks!", count++);
+                if(mLibVLC == null)
+                    mLibVLC = new LibVLC();
+                mLibVLC.PlayMRL("http://www.montemagno.com/sample.mp3");
+            };
+        }
+    }
+}
+
+
